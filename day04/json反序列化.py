bee103efe969bf60a337e54f028ae94ac17b79e0
West <921971961@qq.com>
2019-11-19 22:47:17 +0800

    20191106 day4 finished

--- day04/json反序列化.py
+++ day04/json反序列化.py
@@ -1,7 +1,32 @@
+# import json
+# f = open("test.txt","r")
+#
+# data = json.loads(f.read())
+# print(data.get("name"))
+#
+# f.close()
+
+
+import pickle
+data = {'k1':123,'k2':'hello'}
+#pickle.dumps 将数据通过特殊的形式转换为只有python认识的字符串
+p_str = pickle.dumps(data)
+print(p_str)
+
+print(type(data))
+#pickle.dump 将数据通过特殊的形式转换为只有python认识的字符串，并写入文件
+with open("test.pk",'w') as f:
+    #python3会报错，python3中文件写入模式应为“wb”，使用二进制方式，而不是字符方式写入
+    pickle.dump(data,f)
+
 import json
-f = open("test.txt","r")
 
-data = json.loads(f.read())
-print(data.get("name"))
+j_str = json.dumps(data)
+print(j_str)
 
-f.close()
+#json.dump 将数据通过特殊的形式转换为所有程序语言都认识的字符串，并写入文件
+with open("test.json","w") as f:
+    json.dump(data,f)
+
+
+
--- day04/pickle反序列化.py
+++ day04/pickle反序列化.py
@@ -1,12 +1,6 @@
 import pickle
 
-def sayhi(name):
-
-
-    print("hello2", name)
 f = open("test.txt","rb")
-
 data = pickle.loads(f.read())#等价于data = pickle.load(f)
 print(data["func"]("yanzhuang"))
-
 f.close()
