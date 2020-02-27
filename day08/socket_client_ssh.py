45c7327f4995d2d1c83e31a84a96bfb994c512b8
yz <921971961@qq.com>
2020-02-27 11:33:21 +0800

    day8update1

--- day08/socket_client_ssh.py
+++ day08/socket_client_ssh.py
@@ -12,10 +12,11 @@ while True:
     cmd_res_size = client.recv(1024) #接受命令结果长度
     # cmd_res = client.recv(1024) #虽然写的1024，代表最多是1024，不一定每次都收满
     # print(cmd_res.decode())
+    client.send("准备好接受了，loser可以发了".encode("utf-8"))
     print("命令结果长度：",cmd_res_size)
     received_size = 0
     received_data = b''
-    while received_size < int(cmd_res_size.decode()):
+    while received_size < int(cmd_res_size.decode()): #linux上发送数据长度的时候会默认加上数据
         data = client.recv(1024)
         received_size += len(data) #每次收到的有可能小于1024，所以必须用len判断
         # print(data.decode())
--- day08/socket_server_ssh.py
+++ day08/socket_server_ssh.py
@@ -1,6 +1,6 @@
 
 
-import socket,os
+import socket,os,time
 
 server = socket.socket()
 server.bind(('localhost',9999))
@@ -21,7 +21,12 @@ while True:
         print("before send", len(cmd_res))
         if len(cmd_res) == 0:
             cmd_res = "cmd has no output..."
+        #先发命令大小，再发数据，可能出现粘包，两次发送数据一起
         conn.send(str(len(cmd_res.encode())).encode("utf-8")) #整数不能encode，所以转成字符串
+        #time.sleep(0.5) #将两条命令隔开
+        client_ack = conn.recv(1024) #wait client to confirm 解决粘包的小方法
+        print("client_ack:",client_ack.decode())
         conn.send(cmd_res.encode("utf-8")) #没有全部发给客户端的会放在缓冲区，下次发自动发缓冲区里的数据
         print("send done")
 server.close()
+
